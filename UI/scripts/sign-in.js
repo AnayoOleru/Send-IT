d55747a417cb2a054745f532033318bb716505e6
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-02 06:18:39 +0100

    re-structured index for gh-pages

--- UI/scripts/sign-in.js
+++ UI/scripts/sign-in.js
@@ -1,15 +1,10 @@
 // Get the modal
-var modal = document.getElementByClassName('contain');
-
-// When the user clicks the button, open the modal 
-function btn() {
-    modal.style.visibility = "visible";
-}
+let modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.visibility = "hidden";
+        modal.style.display = "none";
     }
 }
 
