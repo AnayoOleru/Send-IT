6a11562a34c18e38c14bda03b6a91f32fc45922b
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-06 22:48:55 +0100

    feature(POST-users): add dummy data to orders

---
+++ src/db/orders.js
@@ -0,0 +1,10 @@
+export default {
+  1: {
+    id: 1,
+    parcelWeight: '20kg',
+    price: '#15000',
+    departure: 'Lagos',
+    destination: 'Abuja',
+    pickupLocation: '33 send-it building FCT'
+  }
+};
