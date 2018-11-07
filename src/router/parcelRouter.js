b1137c9e5771317eb150c3f8288bfee4797ffad2
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-07 21:26:39 +0100

    feature(GET-parcels):add router endpoints

--- src/router/parcelRouter.js
+++ src/router/parcelRouter.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const {
   getParcels, createParcel, getParcelById, cancelParcel
 } = parcels;
-// router to fetch all parcel delivery orders
+// import the logic from parcelcontroller and ...
+// fetch all parcel delivery orders
+// create delivery new delivery
 router
   .route('')
   .get(getParcels)
   .post(createParcel);
-// route cancel the specific parcel delivery order
+// cancel the specific parcel delivery order
 router
   .route('/:parcelId/cancel')
   .put(cancelParcel);
