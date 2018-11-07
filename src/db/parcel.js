d45efcb29fa3875856208fbfbbfb102dee340812
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-07 07:30:08 +0100

    feature(POST-parcels):add an endpoint for post parcels

---
+++ src/db/parcel.js
@@ -0,0 +1,12 @@
+module.exports = {
+  14590: {
+    id: '14590',
+    userId: '45547',
+    parcelWeight: '20kg',
+    price: '#15000',
+    departure: 'Lagos',
+    destination: 'Abuja',
+    pickupLocation: '33 send-it building FCT',
+    status: 'proccessing'
+  }
+};
