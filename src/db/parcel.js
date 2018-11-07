e9089afb358d9483b149da06d818d53d4de1758e
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-08 00:15:22 +0100

    added another dummy object percel data for test

--- src/db/parcel.js
+++ src/db/parcel.js
@@ -8,5 +8,16 @@ module.exports = {
     destination: 'Abuja',
     pickupLocation: '33 send-it building FCT',
     status: 'proccessing'
+  },
+
+  12345: {
+    id: '12345',
+    userId: '45457',
+    parcelWeight: '20kg',
+    price: '#120000',
+    departure: 'Lagos',
+    destination: 'Abuja',
+    pickupLocation: '33 send-it building FCT',
+    status: 'proccessing'
   }
 };
