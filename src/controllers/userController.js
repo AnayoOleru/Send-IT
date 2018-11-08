95627538a7b17970a120811274954c3c3e8fa646
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-08 03:13:15 +0100

    feature(GET-users):add logic to get all parcels from user and comment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,14 @@
 import { randomBytes } from 'crypto';
 import users from '../db/users';
+import parcels from '../db/parcel';
 
 export default {
+  // logic to get all users
   getUsers(req, res) {
     return res.json(users);
   },
+  // gets all parcels under specific user
+  userParcels(req, res) { return res.json(parcels); },
 
   createUser(req, res) {
     const { name = '', email, password } = req.body;
