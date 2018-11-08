c8857cf077939a4596f76eb29070e839f028dd96
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-08 03:23:20 +0100

    feature(GET-users):add user parcels routes

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -2,11 +2,15 @@ import express from 'express';
 import users from '../controllers/userController';
 
 const router = express.Router();
-const { getUsers, createUser } = users;
+const { getUsers, createUser, userParcels } = users;
 
 router
   .route('/users')
   .get(getUsers)
   .post(createUser);
+
+router
+  .route('/:userId/parcels')
+  .get(userParcels);
 
 export default router;
