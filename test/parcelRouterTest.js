cec84564426b6465d31d2d92714325df87e36f2f
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-09 12:02:29 +0100

    feature(test-PUT/cancel-parcels):add test to endpoint

--- test/parcelRouterTest.js
+++ test/parcelRouterTest.js
@@ -9,13 +9,13 @@ use(chaiHttp);
 
 const { expect } = chai;
 const should = _should(); // eslint-disable-line
-let requester = null;
+const requester = null;
 const ENDPOINT = '/api/v1/parcels';
 
 describe('/api/v1/parcels', () => {
-  let parcels = 0;
-  let userToken = null;
-  let adminToken = null;
+  const parcels = 0;
+  const userToken = null;
+  const adminToken = null;
 
   describe('GET /', () => {
     it('should get all parcels', (done) => {
@@ -29,5 +29,28 @@ describe('/api/v1/parcels', () => {
     });
   });
 
- 
+  // to change the status of delivery order
+  describe('PUT /:id', () => {
+    it('should not update order without status param in req.body', (done) => {
+      requester
+        .put(`${ENDPOINT}/1`)
+        .then((res) => {
+          res.status.should.be.equal(400);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should update order status', (done) => {
+      requester
+        .put(`${ENDPOINT}/1`)
+        .send({ status: 'decline' })
+        .then((res) => {
+          res.status.should.be.equal(200);
+          expect(res.body).to.have.property('status', 'decline');
+          done();
+        })
+        .catch(done);
+    });
+  });
 });
