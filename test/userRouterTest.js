543227a8585e7a47641dd91220085fac621b4edb
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-09 12:22:42 +0100

    feature(test-GET//parcels/id):add test to endpoint

---
+++ test/userRouterTest.js
@@ -0,0 +1,43 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../src/server';
+import store from '../src/store';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+const should = chai.should(); // eslint-disable-line
+const requester = null;
+const ENDPOINT = '/api/v1/users';
+
+describe('/api/v1/users', () => {
+  const userToken = null;
+  const adminToken = null; //eslint-disable-line
+
+  describe('GET /:id/orders', () => {
+    before(() => {
+      store.orders = {}; // clear orders
+    });
+
+    it('should error for unauthorized users', (done) => {
+      requester
+        .get(`${ENDPOINT}/12233/orders`)
+        .then((res) => {
+          expect(res.status).to.equal(401);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should get order history for authenticated user', (done) => {
+      requester
+        .get(`${ENDPOINT}/12233/orders`)
+        .set('Authorization', userToken)
+        .then((res) => {
+          res.body.should.be.a('array').that.is.empty; // eslint-disable-line
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
