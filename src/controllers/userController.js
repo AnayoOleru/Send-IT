9cd5e1b5a622e06875d8f6383701537ff321a9d9
Anayo Oleru <anayo_oleru@outlook.com>
2018-11-16 16:10:58 +0100

    [chore 161973247] add  jsDoc comments

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,12 +7,7 @@ import parcels from '../db/parcel';
  * @class userController
 */
 class UserController {
-  /**
-   * @staticmethod
-   * @param {object} req - Request Object
-   * @param {object} res - Response Object
-   * @returns {object} - Returns all parcels object
-   */
+  
   static getUsers(req, res) {
     return res.json(users);
   }
